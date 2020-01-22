081b0dcd8df728e3aa5e7b5fff9c98a82a858867
Fabio Romanini <fabioaromanini@gmail.com>
2020-01-22 02:56:13 -0200

    creates and populates sql database tables

---
+++ src/db/initialize.js
@@ -0,0 +1,51 @@
+const alasql = require('alasql');
+
+alasql(
+  `CREATE TABLE credit_card_transactions (
+  transaction_id STRING,
+  credit_card STRING
+);
+`
+);
+
+alasql(
+  `CREATE TABLE transaction_status_score (
+  transaction_status STRING,
+  score NUMBER
+);
+`
+);
+
+alasql(
+  `CREATE TABLE transaction_status (
+  transaction_id STRING,
+  status STRING
+);
+`
+);
+
+alasql(
+  `INSERT INTO credit_card_transactions VALUES 
+("transacao1","9454832-5069"),
+("transacao2","9454832-5069"),
+("transacao3","9454832-5069"),
+("transacao4","9454832-5069");
+`
+);
+
+alasql(
+  `INSERT INTO transaction_status_score VALUES 
+("approved",-1),
+("pending",0),
+("declined",5);
+`
+);
+
+alasql(
+  `INSERT INTO transaction_status VALUES 
+("transacao1", "approved"),
+("transacao2", "approved"),
+("transacao3", "pending"),
+("transacao4", "declined");
+`
+);
