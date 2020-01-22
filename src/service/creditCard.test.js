5ed28370597665a299ec7289379478f545ac21a0
Fabio Romanini <fabioaromanini@gmail.com>
2020-01-22 12:42:21 -0200

    adds test for credit card number happy flow

--- src/service/creditCard.test.js
+++ src/service/creditCard.test.js
@@ -1,5 +1,12 @@
-describe('getCreditCardNumber', () => {
-  test('Evergreen mocked test', () => {
-    expect(1).toBe(1);
+const creditCardService = require('./creditCard');
+
+describe('GetCreditCardNumber', () => {
+  test('Returns credit card number for valid credit card', () => {
+    const creditCard = {
+      bin: '9454832',
+      last4: '5069',
+    };
+
+    expect(creditCardService.getCreditCardNumber(creditCard)).toMatch('9454832-5069');
   });
 });
