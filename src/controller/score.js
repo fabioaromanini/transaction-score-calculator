2ed52d383ebb59e5cba9b9d1cbc848dd7b3e3482
Fabio Romanini <fabioaromanini@gmail.com>
2020-01-22 03:40:15 -0200

    assigns random score given credit card number

--- src/controller/score.js
+++ src/controller/score.js
@@ -2,10 +2,12 @@ const creditCardService = require('../service/creditCard');
 
 module.exports = {
   createScore: (totalAmount, cpf, creditCard) => {
-    const creditCardScore = creditCardService.getCreditCardNumber(creditCard);
+    const creditCardNumber = creditCardService.getCreditCardNumber(creditCard);
+    console.log(`Calculating score for ${creditCardNumber}`);
+    const creditCardScore = creditCardService.getCreditCardScore(creditCardNumber);
     return {
       TOTAL_AMOUNT: totalAmount,
-      CC_SCORE: Math.random() * 10,
+      CC_SCORE: creditCardScore,
       CPF_RATING: Math.random(),
     };
   },
--- src/service/creditCard.js
+++ src/service/creditCard.js
@@ -1,3 +1,4 @@
 module.exports = {
   getCreditCardNumber: creditCard => `${creditCard.bin}-${creditCard.last4}`,
+  getCreditCardScore: creditCardNumber => Math.random() * 10,
 };
