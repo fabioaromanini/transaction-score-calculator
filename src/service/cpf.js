0120dc7fc0137d07eeec6a5a1110ba621dfe3523
Fabio Romanini <fabioaromanini@gmail.com>
2020-01-22 14:33:22 -0200

    implements cpfService

--- src/service/cpf.js
+++ src/service/cpf.js
@@ -1,20 +1,17 @@
-const axios = require('axios');
-
-const { TRAMPO_CERTO_URL } = process.env;
-
-const cpfRatingsClient = axios.create({
-  baseURL: TRAMPO_CERTO_URL,
-});
+const calculateScore = (min, max, inclusive) => {
+  if (inclusive) return Math.floor(Math.random() * (max - min + 1)) + min;
+  else return Math.random() * (max - min) + min;
+};
 
 module.exports = {
-  getCpfRating: async cpf => {
-    const {
-      data: { rating: cpfRating },
-    } = await cpfRatingsClient.get('/cpf_query', {
-      params: {
-        cpf,
-      },
-    });
-    return cpfRating;
+  getNumericRating: rating => {
+    switch (rating) {
+      case 'good':
+        return calculateScore(0, 0.5);
+      case 'bad':
+        return calculateScore(0.50000000000001, 1);
+      default:
+        return null;
+    }
   },
 };
